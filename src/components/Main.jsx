17c2858e66426299775125a5cb3ace20a64bdafe
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-15 10:12:02 -0600

    added table and dummydata

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dummyContacts from '../dummyData/index.js';
+
+console.log(dummyContacts)
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
+
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+            {
+              contacts.map((contact, idx) => {
+                return (
+                <tr key={`contact table row: ${idx}`}>
+                  <td>{contact.name}</td>
+                  <td>{contact.phone}</td>
+                  <td>{contact.email}</td>
+                </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
